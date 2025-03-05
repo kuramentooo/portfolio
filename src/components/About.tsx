61e0e0953adc81263bd627b4883eb508fa2b7d5c
Clement <cdelaunoy@luminess.eu>
2025-03-05 14:39:52 +0100

    ADD: all

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { useState } from 'react'
 import { profile } from '@/data/profile'
 
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Image from 'next/image'
 import { profile } from '@/data/profile'
 

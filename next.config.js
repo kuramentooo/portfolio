0f78e9328e76abdc34526738669855a8d69a7a8d
Clement <cdelaunoy@luminess.eu>
2025-03-05 14:07:53 +0100

    ADD: all

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: '',
-  assetPrefix: '',
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio/',
   trailingSlash: true,
 }
 
